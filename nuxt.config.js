a97e5332d38d5db56751ab2f85381df0aa55e573
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2023-03-16 11:10:56 +0900

    first

---
+++ nuxt.config.js
@@ -0,0 +1,27 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: "static",
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "mbti-fast-campus",
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  css: ["~/assets/css/global.css"],
+  plugins: [],
+  components: true,
+  buildModules: [],
+  modules: [],
+  build: {}
+};
